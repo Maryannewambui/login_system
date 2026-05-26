0e503420243e258b5d514dc8d1d94907f745f2aa
Maryannewambui <trimbleleewambui@gmail.com>
2026-05-26 14:49:26 +0300

    complete version 2

--- authenticate.php
+++ authenticate.php
@@ -18,7 +18,7 @@ if ($stmt = $conn->prepare('SELECT id, password FROM accounts WHERE username = ?
         $stmt->fetch();
 
         // Verify the password
-        if (password_verify($_POST['password'], $password)) {
+        if ($password !== null && password_verify($_POST['password'], $password)) {
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
